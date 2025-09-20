dc049065ad39f78347b1b51944396b843ac5cab6
Marc Haye <marc.antonio.haye@gmail.com>
2025-09-20 17:05:43 +0200

    refactor: restruct hello demo

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -8,26 +8,22 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class DemoController extends AbstractController
 {
-    #[Route('/hello', name: 'app_demo', methods: ["GET"])]
+    #[Route('/hello', name: 'hello', methods: ["GET"])]
     public function hello(): Response
     {
         return $this->render('demo/hello.html.twig');
     }
 
-    #[Route('/hello/{name}', name: 'get_hello', methods: ["GET"])]
+    #[Route('/hello/{name}', name: 'hello2', methods: ["GET"])]
     public function hello2(string $name): Response
     {
-        return $this->render('demo/hello2.html.twig', [
-            'name' => $name
-        ]);
+        return $this->render('demo/hello2.html.twig', ['name' => $name]);
     }
 
-    #[Route('/hello/courses', name: 'get_courses', methods: ["GET"])]
+    #[Route('/hello/courses', name: 'hello3', methods: ["GET"])]
     public function hello3(): Response
     {
         $courses = array("patates","fromage","chocolat","frites");
-        return $this->render('demo/hello2.html.twig', [
-            'courses' => $courses
-        ]);
+        return $this->render('demo/hello3.html.twig', ['courses' => $courses]);
     }
 }
