ea774c380eb2860c6c1d3daf249eb972e1f09f76
Marc Haye <marc.antonio.haye@gmail.com>
2025-09-20 13:41:21 +0200

    feat: add file Auth Subscriber

---
+++ src/EventSubscriber/AuthenticationSubscriber.php
@@ -0,0 +1,11 @@
+<?php
+
+ use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+ use Symfony\Component\HttpFoundation\RequestStack;
+ use Symfony\Component\Security\Http\Event\LoginFailureEvent;
+ use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
+ use Symfony\Component\Security\Http\Event\LogoutEvent;
+ 
+class AuthenticationSubscriber {
+
+}
