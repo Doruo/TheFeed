32bcacf68be27f8c3124bbd0e84832729ca7043d
Marc Haye <marc.antonio.haye@gmail.com>
2025-09-25 14:56:56 +0200

    feat: logout

--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -12,8 +12,6 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
  use Symfony\Component\Validator\Constraints\File;
  use Symfony\Component\Validator\Constraints\Length;
- use Symfony\Component\Validator\Constraints\NotBlank;
- use Symfony\Component\Validator\Constraints\NotNull;
  use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -34,8 +32,6 @@ class UtilisateurType extends AbstractType
             [
                 "mapped" => false,
                 "constraints" => [
-                    new NotBlank(),
-                    new NotNull(),
                     new Length(min:8, max:30, minMessage:"Il faut au moins 8 caractères!",maxMessage:"Il faut moins de 30 caractères!"),
                     new Regex(pattern : '#^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,30}$#', 
                     message : 'Format non respecté'),
